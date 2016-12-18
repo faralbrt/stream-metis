c33b606912c2388b271c95094f6dcfaed80a0713
faralbrt <albert.farhi5@gmail.com>
2016-12-18 15:05:50 -0500

    add asin_id to log table

--- db/migrate/20161218145227_create_asin_logs.rb
+++ db/migrate/20161218145227_create_asin_logs.rb
@@ -1,6 +1,7 @@
 class CreateAsinLogs < ActiveRecord::Migration
   def change
     create_table :asinlogs do |t|
+      t.integer :asin_id, null: false
       t.string :title, null: false
       t.float :price, null: false
       t.float :cost
