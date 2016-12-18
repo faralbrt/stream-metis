23ebfa483a53ed6da7d8a34698c2825f5c4a0381
faralbrt <albert.farhi5@gmail.com>
2016-12-18 15:17:47 -0500

    rename table to asin_logs in migration

--- db/migrate/20161218145227_create_asin_logs.rb
+++ db/migrate/20161218145227_create_asin_logs.rb
@@ -1,6 +1,6 @@
 class CreateAsinLogs < ActiveRecord::Migration
   def change
-    create_table :asinlogs do |t|
+    create_table :asin_logs do |t|
       t.integer :asin_id, null: false
       t.string :title
       t.float :price, null: false
