90b537b101b2994706c86dbcd9dd9a506bf0739d
faralbrt <albert.farhi5@gmail.com>
2016-12-18 22:43:01 -0500

    errors and form for new asin and multiple asins

--- app/controllers/asins.rb
+++ app/controllers/asins.rb
@@ -3,6 +3,20 @@ get '/asins/today' do
   erb :'asins/_table', layout: true
 end
 
+get '/asins/new' do
+  erb :'asins/new'
+end
+
+post '/asins' do
+  @asin = Asin.new(name: params[:name])
+  if @asin.save
+    redirect '/'
+  else
+    @errors = @asin.errors.full_messages
+    erb :'asins/new'
+  end
+end
+
 get '/asins/:asin' do
   @asin_logs = AsinLog.find_by_asin_name(params[:asin])
   erb :'asins/_table', layout: true
