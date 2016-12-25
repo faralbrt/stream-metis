50a08f3daf59e1af84fc00852e8cbf23461959eb
faralbrt <albert.farhi5@gmail.com>
2016-12-25 00:54:52 -0500

    add fluctuation to view

--- app/models/asin_log.rb
+++ app/models/asin_log.rb
@@ -16,11 +16,11 @@ class AsinLog < ActiveRecord::Base
 
   def fluctuation
     prev_log = AsinLog.where("asin_id = ? AND created_at < ?", self.asin.id, self.created_at)
-    return "inherit" if prev_log.empty?
+    return "none" if prev_log.empty?
     prev_log = prev_log.max_by{|log| log.created_at}
-    return "blue" if prev_log.price == 0.0 || self.price == 0.0
+    return "blue" if prev_log.price == 0.0 && self.price == 0.0
     return "green" if self.price/prev_log.price > 1.029
     return "red" if self.price/prev_log.price < 0.971
-    return "inherit"
+    return "none"
   end
 end
