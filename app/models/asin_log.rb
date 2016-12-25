cc04a736bc059907100929eb0c62ccb4089ee6c9
faralbrt <albert.farhi5@gmail.com>
2016-12-25 00:50:04 -0500

    add fluctuation method

--- app/models/asin_log.rb
+++ app/models/asin_log.rb
@@ -13,4 +13,14 @@ class AsinLog < ActiveRecord::Base
     asin_logs = AsinLog.where(asin: asin)
     asin_logs.sort_by{ |asin_log| asin_log.created_at }
   end
+
+  def fluctuation
+    prev_log = AsinLog.where("asin_id = ? AND created_at < ?", self.asin.id, self.created_at)
+    return "inherit" if prev_log.empty?
+    prev_log = prev_log.max_by{|log| log.created_at}
+    return "blue" if prev_log.price == 0.0 || self.price == 0.0
+    return "green" if self.price/prev_log.price > 1.029
+    return "red" if self.price/prev_log.price < 0.971
+    return "inherit"
+  end
 end
